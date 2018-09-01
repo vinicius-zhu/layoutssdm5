c0c80704e02208734921e62c5eabd33eab01f3c1
Vinicius <vinicius.zhu@aluno.ifsp.edu.br>
2018-08-31 22:24:17 -0300

    Implementação de save/restore states e botão limpar

--- app/src/main/java/br/edu/ifsp/scl/sdm/layoutssdm/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/sdm/layoutssdm/MainActivity.java
@@ -5,19 +5,37 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+    private final String ESTADO_NOTIFICACAO_CHECKBOX = "ESTADO_NOTIFICACAO_CHECKBOX";
+    private final String NOTIFICACAO_RADIOBUTTON_SELECIONADO = "NOTIFICACAO_RADIOBUTTON_SELECIONADO";
+
+
     private CheckBox notificacoesCheckBox;
     private RadioGroup notificacoesRadioGroup;
+    private RadioButton emailRadioButton;
+    private RadioButton telefoneRadioButton;
+    private EditText nomeEditText;
+    private EditText telefoneEditText;
+    private EditText emailEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.relative_layout_activity_main);
 
+
         notificacoesCheckBox = findViewById(R.id.notificacoesCheckBox);
         notificacoesRadioGroup = findViewById(R.id.notificacoesRadioGroup);
+        emailRadioButton = findViewById(R.id.emailRadioButton);
+        telefoneRadioButton = findViewById(R.id.telefoneRadioButton);
+        nomeEditText = findViewById(R.id.nomeEditText);
+        telefoneEditText = findViewById(R.id.telefoneEditText);
+        emailEditText = findViewById(R.id.emailEditText);
 
         // Exibindo/ocultando RadioGroup por evento de Click Listener
         /*
@@ -35,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });*/
 
+        // Exibindo/ocultando RadioGroup por evento de On Checked Change Listener
         notificacoesCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -50,5 +69,30 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(ESTADO_NOTIFICACAO_CHECKBOX, notificacoesCheckBox.isChecked());
+        outState.putInt(NOTIFICACAO_RADIOBUTTON_SELECIONADO, notificacoesRadioGroup.getCheckedRadioButtonId());
+    }
 
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        notificacoesCheckBox.setChecked(savedInstanceState.getBoolean(ESTADO_NOTIFICACAO_CHECKBOX, false));
+        int idRadioButtonSelecionado = savedInstanceState.getInt(NOTIFICACAO_RADIOBUTTON_SELECIONADO, -1);
+        if (idRadioButtonSelecionado != -1) {
+            notificacoesRadioGroup.check(idRadioButtonSelecionado);
+        }
+    }
+
+    public void limparFormulario(View botao){
+        notificacoesCheckBox.setChecked(false);
+        emailRadioButton.setChecked(false);
+        telefoneRadioButton.setChecked(false);
+        nomeEditText.setText("");
+        telefoneEditText.setText("");
+        emailEditText.setText("");
+    }
 }
