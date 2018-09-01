af3bc875f6a285b30d5315dbbca08110455340ff
Vinicius <vinicius.zhu@aluno.ifsp.edu.br>
2018-08-31 21:59:06 -0300

    Lógica para esconder/exibir radiogroup

--- app/src/main/java/br/edu/ifsp/scl/sdm/layoutssdm/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/sdm/layoutssdm/MainActivity.java
@@ -2,12 +2,36 @@ package br.edu.ifsp.scl.sdm.layoutssdm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+    private CheckBox notificacoesCheckBox;
+    private RadioGroup notificacoesRadioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.frame_layout_activity_main);
+        setContentView(R.layout.relative_layout_activity_main);
+
+        notificacoesCheckBox = findViewById(R.id.notificacoesCheckBox);
+        notificacoesRadioGroup = findViewById(R.id.notificacoesRadioGroup);
+
+        notificacoesCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (((CheckBox) v).isChecked())
+                {
+                    notificacoesRadioGroup.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    notificacoesRadioGroup.setVisibility(View.GONE);
+                }
+            }
+        });
     }
+
+
 }
