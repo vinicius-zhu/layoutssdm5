8fff692e5e4b8f8abb2e18fc887103eb862d2d42
Vinicius <vinicius.zhu@aluno.ifsp.edu.br>
2018-08-31 22:02:48 -0300

    Ajustando exibição de RadioGroup para lógica de Checked Value

--- app/src/main/java/br/edu/ifsp/scl/sdm/layoutssdm/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/sdm/layoutssdm/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,10 +19,26 @@ public class MainActivity extends AppCompatActivity {
         notificacoesCheckBox = findViewById(R.id.notificacoesCheckBox);
         notificacoesRadioGroup = findViewById(R.id.notificacoesRadioGroup);
 
+        // Exibindo/ocultando RadioGroup por evento de Click Listener
+        /*
         notificacoesCheckBox.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (((CheckBox) v).isChecked())
+                {
+                    notificacoesRadioGroup.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    notificacoesRadioGroup.setVisibility(View.GONE);
+                }
+            }
+        });*/
+
+        notificacoesCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
                 {
                     notificacoesRadioGroup.setVisibility(View.VISIBLE);
                 }
