cf9aa5b7fa4a3a189c25b342fb1ec78113c8b0d1
Vinicius <vinicius.zhu@aluno.ifsp.edu.br>
2018-09-06 20:13:59 -0300

    Criação de TableLayout

--- app/src/main/java/br/edu/ifsp/scl/sdm/layoutssdm/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/sdm/layoutssdm/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.relative_layout_activity_main);
+        setContentView(R.layout.linear_layout_activity_main);
 
 
         notificacoesCheckBox = findViewById(R.id.notificacoesCheckBox);
